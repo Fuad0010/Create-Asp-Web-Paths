8a4c41c1437db70092725cac8ca9496d78d4c06a
Fuad0010 <fuadft@code.edu.az>
2022-06-15 22:49:48 +0400

    feat:added 4 indexes

---
+++ firtstAspApp/Controllers/Home/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace firtstAspApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            //return Content($"Home controller Index action worked {id} ");
+            return View();
+        }
+        public IActionResult About()
+        {
+            //return Content($"Home controller About action worked {id} ");
+            return View();
+        }
+    }
+}
---
+++ firtstAspApp/Controllers/Shop/ShopController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace firtstAspApp.Controllers.Shop
+{
+    public class ShopController : Controller
+    {
+        public IActionResult Cars()
+        {
+            //return Content ($"Shop controller Cars action worked {id}");
+            return View();
+        }
+        public IActionResult Trucks(int id)
+        {
+            //return Content ($"Shop controller Trucks action worked {id}");
+            return View();
+        }
+    }
+}
